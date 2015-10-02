f4a52e7e2516390267bf9a66463b435875518b0b
Ryan Graham <ryangrahamwpb@gmail.com>
2015-10-02 11:25:02 -0400

    update steps_taken controller

--- app/controllers/stepstaken_controller.rb
+++ app/controllers/stepstaken_controller.rb
@@ -48,7 +48,23 @@ def update
 end
 
 
+def destroy
+  @step.destroy
+  respond_to do |format|
+    format.html { redirect_to steps_url, notice: 'Step was successfully destroyed.' }
+    format.json { head :no_content }
+  end
+end
 
 
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_step
+    @step = Stepstaken.find(params[:id])
+  end
 
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def step_params
+    params.require(:step).permit(:taken, :entry_date)
+  end
 end
